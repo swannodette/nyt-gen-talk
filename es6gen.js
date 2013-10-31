142629f084399b1134379e93ffb63edca3e4c456
David Nolen <david.nolen@gmail.com>
2013-10-31 11:34:27 -0400

    details about where yield may appear

--- es6gen.js
+++ es6gen.js
@@ -1,4 +1,6 @@
 // node --harmony es6gen.js
+// traceur --out es6gen.js es6gen.out.js; cat runtime.js es6gen.out.js | node
+// regenerator --include-runtime es6gen.js > es6gen.reg.js; node es6gen.reg.js
 
 function* foo() {
   yield 1;
@@ -20,3 +22,22 @@ state = g.next();
 console.log(state);
 
 // if you call g.next() after done will throw!
+
+
+// yield can appear anywhere an expression can
+// the follow doesn't work with Traceur yet
+/*
+function* bar() {
+  return [1, yield "hello", 2];
+}
+
+var h = bar();
+
+state = h.next();
+
+console.log(state);
+
+state = h.next("there");
+
+console.log(state);
+*/
